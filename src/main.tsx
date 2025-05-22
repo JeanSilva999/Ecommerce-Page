fd7f27924b4c6401b6d834b0d6da512abf10facb
JeanSilva999 <jean.luca.dev@outlook.com>
2025-05-22 11:59:12 -0300

    chore: set ProductProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/main.css";
 import App from "./App.tsx";
+import { ProductProvider } from "./contexts/product-context.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ProductProvider>
+      <App />
+    </ProductProvider>
   </StrictMode>
 );
